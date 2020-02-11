508125c4597a969b2c40b1f4ec3b0b5974598a2d
Jamar Torres <Jetorres88@gmail.com>
2020-02-10 21:20:49 -0500

    Fixed error

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,8 +2,9 @@
 from hashtables import (HashTable,
                         hash_table_insert,
                         #hash_table_remove,
-                        hash_table_retrieve,
-                        #hash_table_resize)
+                        hash_table_retrieve
+                        #hash_table_resize
+)
 
 
 def get_indices_of_item_weights(weights, length, limit):
